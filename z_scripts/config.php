17fbf2b5667b436a9df02251b05a1e6e0a9deb88
Florian Morello <arsonik@me.com>
2019-03-22 09:48:29 +0100

    pristine

---
+++ z_scripts/config.php
@@ -0,0 +1,68 @@
+<?php
+
+return [
+    'respositories' => [
+        'global' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/global/global.git'
+        ],
+        'fusion' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/dating/fusion.git'
+        ],
+        'call' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/global/call.git'
+        ],
+        'ui' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/dating/admin-web-ui.git'
+        ],
+        'tools' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/global/admin-web-tools.git'
+        ],
+        'archi_config' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/global/archi_config.git'
+        ],
+        'archi_config_loader' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/config/archi_config_loader.git'
+        ],
+        'biz' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/prelinker/biz.git'
+        ],
+        'biz_admin' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/prelinker/biz_admin.git'
+        ],
+        'v4' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/dating/v4.git'
+        ],
+        'v5' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/dating/v5.git'
+        ],
+        'pay' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/payment/pay.git'
+        ],
+        'promo' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/prelinker/promo.git'
+        ],
+        'dating_ios' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/dating/dating_ios.git'
+        ],
+        'android-multichannel' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/dating/android-multichannel.git'
+        ],
+        'dblinker' => [
+            'source' => 'https://github.com/ezweb/dblinker.git'
+        ],
+        'redirect' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/prelinker-click-logger/redirect.git'
+        ],
+        'bridghit' => [
+
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/prelinker-click-logger/bridghit.git'
+        ],
+
+        'admin-web-ui' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/dating/admin-web-ui.git'
+        ],
+        'admin-web-tools' => [
+            'source' => 'ssh://git@gitlab.easyflirt.com:222/global/admin-web-tools.git'
+        ],
+
+    ]];
--- z_scripts/git.php
+++ z_scripts/git.php
@@ -1,20 +1,31 @@
 <?php
+$config = include __DIR__ . '/config.php';
 
-$submodules = array_filter(array_map(function($line) {
-    $submodule = explode(' ', trim($line))[1];
-    return $submodule;
-}, array_filter(explode(PHP_EOL, shell_exec('git submodule status')))));
+$action = $_SERVER['argv'][1] ?? null;
 
+foreach ($config['respositories'] as $name => $respository) {
+    switch ($action) {
+        case 'clone':
+            passthru('git clone ' . $respository['source'] . ' ' . $name);
+            break;
+        case 'deinit':
+            echo 'git submodule deinit -f ' . $name . PHP_EOL;
+            break;
 
-foreach ($submodules as $submodule) {
-    chdir(__DIR__ . '/../' . $submodule);
+        case 'status':
+            chdir(__DIR__ . '/../' . $name);
 
-    $output = shell_exec('git st');
-    if (strpos($output, 'Changes') || strpos($output, 'diverged') || strpos($output, 'publish your local commits')) {
-        echo str_repeat('-', 20) . PHP_EOL;
-        echo '   ' . $submodule . PHP_EOL;
-        echo str_repeat('-', 20) . PHP_EOL;
-        echo $output . PHP_EOL;
+            $output = shell_exec('git status');
+            if (strpos($output, 'Changes') || strpos($output, 'diverged') || strpos($output, 'publish your local commits')) {
+                echo str_repeat('-', 20) . PHP_EOL;
+                echo '   ' . $name . PHP_EOL;
+                echo str_repeat('-', 20) . PHP_EOL;
+                echo $output . PHP_EOL;
+            }
+            break;
+
+        default:
+            throw new \Exception('Unknown action');
     }
 }
 
